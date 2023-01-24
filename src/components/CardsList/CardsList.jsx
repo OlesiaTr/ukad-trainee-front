13ffea4b5f16300a110e3538fa025ced0c88dd1c
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-01-24 02:27:01 +0200

    feat: add Home page

---
+++ src/components/CardsList/CardsList.jsx
@@ -0,0 +1,3 @@
+export const CardsList = ({ data }) => {
+  return <></>;
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,39 @@
+// Core
+import { useState, useEffect } from 'react';
+
+// API
+import { fetchBreeds } from './../services/theCatApi';
+
+// Components
+import { Loader } from 'components/Loader';
+import { CardsList } from 'components/CardsList';
+
 const Home = () => {
-  return <></>;
+  const [cats, setCats] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getCats = async () => {
+      try {
+        setLoading(true);
+        const data = await fetchBreeds();
+        setCats(data);
+        setLoading(false);
+      } catch (error) {
+        throw new Error('Something went wrong, oops', error.message);
+      }
+    };
+
+    getCats();
+  }, []);
+
+  return (
+    <main style={{ flex: '1 1 auto' }}>
+      {loading && <Loader />}
+
+      {cats && <CardsList data={cats} />}
+    </main>
+  );
 };
 
 export default Home;
