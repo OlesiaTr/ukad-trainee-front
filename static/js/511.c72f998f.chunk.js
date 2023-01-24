5d73b05bd0d5fed26726dc4af4a6955560e0a729
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-01-24 03:04:50 +0000

    Deploying to gh-pages from @ OlesiaTr/ukad-trainee-front@4ce68856de08c7ef19fdd1db95512a003d0d819a 🚀

--- static/js/511.c72f998f.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[511],{511:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)(a.Fragment,{})}}}]);
-//# sourceMappingURL=511.c72f998f.chunk.js.map
