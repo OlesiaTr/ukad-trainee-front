2ef4007db56903456a03ec54d65a6fca3af6eb76
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-01-30 21:08:24 +0200

    fix: grid template for 320px screen

--- src/components/CardsList/CardsList.styled.js
+++ src/components/CardsList/CardsList.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 export const Container = styled.ul`
   display: grid;
-  grid-template-columns: repeat(1, 350px);
+  grid-template-columns: repeat(1, 320px);
   justify-content: center;
   gap: 40px;
 
